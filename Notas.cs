b4e9c08442b71908603943914c6721eef08c4206
Maicol Siachoque <136031925+MaicolDSC@users.noreply.github.com>
2024-11-22 15:54:35 -0500

    Add files via upload

---
+++ Notas.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromEje1
+{
+    internal class Notas
+    {
+        double promedio;  //atributos
+        int aprobados;
+        double[] notasestudiantes;
+
+        public Notas(int n)
+        {
+            notasestudiantes = new double[n];
+        }
+        public void Setnotas() 
+        {
+            for(int i = 0; i < notasestudiantes.Length; i++) 
+            {
+                Console.Write($"Nota {i + 1} ingrese el valor: ");
+                notasestudiantes[i] = ValidarNota();
+                //calcular cuantas notas fueron ingresadas y se repite con un ciclo for
+            }
+        }//metodos
+        public double Getpromedio()
+        {
+            promedio = 0;
+            double suma = 0;
+            for (int i = 0; i < notasestudiantes.Length; i++)
+            {
+                suma += notasestudiantes[i];
+            }
+            promedio = suma / notasestudiantes.Length;
+            return promedio;
+            //suma la cantidad de notas ingresadas dividido en la cantidad de estudiantes 
+        }
+        public int Getaprobados()
+        {
+            aprobados = 0;
+            for (int i = 0; i < notasestudiantes.Length; i++)
+            {
+                if (notasestudiantes[i] >= 3.0) 
+                {
+                    aprobados++;
+                }
+            }
+            return aprobados;
+        }
+        public void Listadonotas() // nos muestra cuantas notas son 
+        {
+            for(int i = 0; i < notasestudiantes.Length; i++) 
+            {
+                Console.WriteLine($"Nota estudiante {i + 1}: {notasestudiantes[i]}");
+            }
+        }
+        public int Getdesaprobados() 
+        {
+            return notasestudiantes.Length - aprobados;
+        }
+        private bool CalificacionValida(double n) 
+        {
+            return (n >= 0 && n <= 5.0);
+        }
+
+        private double ValidarNota()
+        {
+            double nota;
+            bool error=true;
+            while (error) 
+            {
+                nota=double.Parse(Console.ReadLine());
+                if (CalificacionValida(nota)) 
+                {
+                    error=false;
+                }
+                else 
+                {
+                    Console.WriteLine("Digite una nota en el rango indicado");
+                    error=true;
+                    
+                }
+                return nota;
+            }
+            return 0;
+        
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿/* 1.	Diseñe un programa que permita capturar n valores de notas de una prueba en una institución educativa así:
+ • Por consola solicita el número de notas ha ingresar 
+• Se ingresan las n notas y como salida debe indicar el promedio de notas. 
+• Se debe evaluar usando condicionales cuantos estudiantes pasan y cuantos no. 
+• La valoración es de 1.0 a 5.0 siendo la nota aprobatoria 3.0
+
+Nombre: Maicol David Siachoque Cubides
+Grupo: 104
+Programa: Ingenieria de sistemas
+Codigo funte: Autoria propia
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromEje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.WriteLine("Este programa sirve para capturas notas de una prueba");
+            Console.WriteLine("Calcula el promedio y cuantos estudiantes pasaron y cuantos perdieron");
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.Write("Ingrese la cantidad de notas que desea comprobar: ");
+            int cantidad = int.Parse(Console.ReadLine());
+          
+
+            Notas notas=new Notas(cantidad); //adjuntamos nuestra clase notas
+
+            notas.Setnotas();
+            Console.WriteLine("Presione Enter para continuar");
+            Console.ReadLine();
+            
+            
+            notas.Listadonotas();
+            
+
+            Console.WriteLine($"El promedio general de las notas ingresadas fue :{notas.Getpromedio()}");
+
+            Console.WriteLine($"Los estudiantes que aprobaron fueron: {notas.Getaprobados()}");
+
+            Console.WriteLine($"Los estudiantes que reprobaron fueron: {notas.Getdesaprobados()}");
+
+            Console.ReadKey();
+        }
+    }
+}
